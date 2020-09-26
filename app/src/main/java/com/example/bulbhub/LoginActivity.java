5621ecc9c22a1f7e83838e6344eda123914c75e7
prabodhalahiru <prabodhalahiru119@gmail.com>
2020-09-26 14:59:42 +0530

    Upto Home - Mazda 3

--- app/src/main/java/com/example/bulbhub/LoginActivity.java
+++ app/src/main/java/com/example/bulbhub/LoginActivity.java
@@ -150,7 +150,7 @@ public class LoginActivity extends AppCompatActivity {
                                  Toast.makeText(LoginActivity.this,"Successfully Logged in",Toast.LENGTH_SHORT).show();
                                  loadingBar.dismiss();
 
-                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, MazdaActivity.class);
                                 //Prevalent.currentOnlineUser = usersData; //newly
                                 startActivity(intent);
                             }
